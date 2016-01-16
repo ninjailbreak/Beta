b786cfd13e9bfa61e98f9fde2db6f4bb33f97ea9
ninjailbreak <ninjailbreak1@gmail.com>
2016-01-16 22:16:36 +0200

    Update CD_Eject.py

--- CD_Eject.py
+++ CD_Eject.py
@@ -4,10 +4,34 @@
  *
  * log
  * -----------
+ * version 1.2 by ninjailbreak, 16.1.16
  * version 1.1 by ninjailbreak, 16.1.16
  * version 1.0 by ninjailbreak, 15.1.16
 '''
 
 from ctypes import windll
-
-windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0 , None)
+from os import system
+from time import *
+def menu():
+    system("cls")
+    print "Welcome to CD Eject!"
+    ans = input("""So what are you want to do?
+    1. Open
+    2. Close
+    3. Exit
+    """)
+    print
+    print
+    if ans == 3:
+        system("cls")
+        print "Good Bye!"
+        exit()
+    elif ans == 1:
+        print "Open Now!"
+        windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0 , None)
+    elif ans == 2:
+        print "Close Now!"
+        windll.WINMM.mciSendStringW(u"set cdaudio door closed", None, 0 , None)
+    sleep(0.2)
+while True:
+    menu()
