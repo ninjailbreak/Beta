7f43c082a07ca2272cc9bca5492e9f2c5d553e04
ninjailbreak <ninjailbreak1@gmail.com>
2016-01-21 15:39:45 +0200

    Update CD_Eject.py

--- CD_Eject.py
+++ CD_Eject.py
@@ -4,36 +4,62 @@
  *
  * log
  * -----------
+ * version 1.4 by ninjailbreak, 20.1.16 [now on linux]
  * version 1.3 by ninjailbreak, 18.1.16
  * version 1.2 by ninjailbreak, 16.1.16
  * version 1.1 by ninjailbreak, 16.1.16
  * version 1.0 by ninjailbreak, 15.1.16
 '''
 
-from ctypes import windll
 from os import system
 from time import *
-def menu():
-    system("cls")
-    print "Welcome to CD Eject!"
-    ans = input("""So what are you want to do?
+
+
+
+def menu(i):
+    if i == 0:
+        system("clear") 
+        print "Welcome to CD Eject!"
+        ans = input("""So what are you want to do?
     1. Open
     2. Close
     3. Exit
     """)
-    print
-    print
-    if ans == 3:
+        print
+        print
+        if ans == 3:
+            system("clear")
+            print "Good Bye!"
+            exit()
+        elif ans == 1:
+            print "Open Now!"
+            system("eject cdrom")
+        elif ans == 2:
+            print "Close Now!"
+            system("eject -t cdrom")
+        sleep(0.2)
+    elif i == 1:
+        from ctypes import windll
         system("cls")
-        print "Good Bye!"
-        exit()
-    elif ans == 1:
-        print "Open Now!"
-        windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0 , None)
-    elif ans == 2:
-        print "Close Now!"
-        windll.WINMM.mciSendStringW(u"set cdaudio door closed", None, 0 , None)
-    sleep(0.2)
-system("color A")
+        print "Welcome to CD Eject!"
+        ans = input("""So what are you want to do?
+    1. Open
+    2. Close
+    3. Exit
+    """)
+        print
+        print
+        if ans == 3:
+            system("cls")
+            print "Good Bye!"
+            exit()
+        elif ans == 1:
+            print "Open Now!"
+            windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0 , None)
+        elif ans == 2:
+            print "Close Now!"
+            windll.WINMM.mciSendStringW(u"set cdaudio door closed", None, 0 , None)
+        sleep(0.2)
+
 while True:
-    menu()
+    menu(system("clear"))
